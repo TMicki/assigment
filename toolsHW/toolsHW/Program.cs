e5b4714b44e2e8eafbdbb4ee6915c43aece94f4c
Thiago Micki <740972@faulkner.edu>
2022-11-01 13:48:42 -0500

    additional conversions

--- toolsHW/toolsHW/Program.cs
+++ toolsHW/toolsHW/Program.cs
@@ -33,6 +33,22 @@ namespace toolsHW
             {
                 Console.WriteLine(value * 2.54 + "cm");
             }
+            if (unit == "km" && unitTo == "Mile")
+            {
+                Console.WriteLine(value + 0.621 + "Mile");
+            }
+            if (unit == "Mile" && unitTo == "km")
+            {
+                Console.WriteLine(value * 1.6 + "km");
+            }
+            if (unit == "m" && unitTo == "yard")
+            {
+                Console.WriteLine(value * 1.09 + "yard");
+            }
+            if (unit == "yard" && unitTo == "m")
+            {
+                Console.WriteLine(value * 0.91 + "m");
+            }
             Console.ReadLine();
         }
     }
